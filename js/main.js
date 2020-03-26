d785749a25191614eee22f4d2dce8b5d568d4375
Antonella Cino <antonella.cino18@gmail.com>
2020-03-26 11:16:21 +0100

    esercizio ok

--- js/main.js
+++ js/main.js
@@ -6,19 +6,22 @@
 
 
 //devo creare un ciclo, partendo da uno, dove controllare per ogni numero, fino ad un massimo di 100 se :
-
+for (var i = 1; i <= 100; i++) {
 // il numero è divisilie x3 e x5
-//se è si, stamperò la stringa "FizzBuzz"
-//se è no, passerò al controllo successivo
-
-//non è divisibile x nessuno dei 2?
-//allora scrivo se stesso
-
-
-//è divisibile x3?
-//se si, stamerò “Fizz” al posto del numero
-//se è no, passerò al controllo successivo
-
-//è divisibile x5?
-//se si, stamerò “Buzz” al posto del numero
-//se è no, passerò al controllo successivo
+  if ( i % 3 ===0 &&  i % 5 ===0) {
+    //se è si, stamperò la stringa "FizzBuzz"
+    console.log("FizzBuzz");
+    //se è no, passerò al controllo successivo
+  } else if ( i % 3 > 0 &&  i % 5 > 0) {//non è divisibile x nessuno dei 2?
+    //allora scrivo se stesso
+    console.log(i);
+    //è divisibile x3?
+  } else if (i % 3 ===0) {
+    //se si, stamerò “Fizz” al posto del numero
+    console.log("Fizz");
+    //se è no, passerò al controllo successivo
+  } else if (i % 5 ===0) {//è divisibile x5?
+    //se si, stamerò “Buzz” al posto del numero
+    console.log("Buzz");
+  }
+}
