c3ee75773249bd19a48d82e87b1312558ec3e3e3
Antonella Cino <antonella.cino18@gmail.com>
2020-03-26 10:38:07 +0100

    analisi

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//Fizz Buzz test:
+//Scrivi un programma che stampi i numeri da 1 a 100,
+//1.per i multipli di 3 stampi “Fizz” al posto del numero
+//2. per i multipli di 5 stampi “Buzz”.
+//3. Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
+
+
+//devo creare un ciclo, partendo da uno, dove controllare per ogni numero, fino ad un massimo di 100 se :
+
+// il numero è divisilie x3 e x5
+//se è si, stamperò la stringa "FizzBuzz"
+//se è no, passerò al controllo successivo
+
+//non è divisibile x nessuno dei 2?
+//allora scrivo se stesso
+
+
+//è divisibile x3?
+//se si, stamerò “Fizz” al posto del numero
+//se è no, passerò al controllo successivo
+
+//è divisibile x5?
+//se si, stamerò “Buzz” al posto del numero
+//se è no, passerò al controllo successivo
